4d5113f5ead52535c2a45926f0ac1bcbb895f99c
xisoDev <xiso@xiso.co.kr>
2016-10-13 22:03:53 +0900

    아이콘,스플래시작업/sqlite 설치

--- www/js/panelCtrl.js
+++ www/js/panelCtrl.js
@@ -1,18 +1,25 @@
 xpos.controller('PanelCtrl', function ($scope, $state, MultipleViewsManager, MessageService) {
-    $scope.messages = MessageService.all();
-    $scope.selectedMessageId = 0;
+    $scope.mainField = "";
 
-    if (MultipleViewsManager.isActive()) {
-        MultipleViewsManager.updateView('view-message', { messageId: $scope.selectedMessageId });
+    $scope.inputField = function(data){
+        $scope.mainField = $scope.mainField + data;
     }
 
-    $scope.changeMessage = function (message) {
-        $scope.selectedMessageId = message.id;
-        console.log(MultipleViewsManager.isActive());
-        if (MultipleViewsManager.isActive()) {
-            MultipleViewsManager.updateView('view-message', { messageId: message.id });
-        } else {
-            $state.go('viewMessage', { messageId: message.id });
-        }
-    };
-})
+    $scope.mainFieldInit = function(){
+        $scope.mainField = "";
+    }
+
+    // if (MultipleViewsManager.isActive()) {
+    //     MultipleViewsManager.updateView('view-message', { messageId: $scope.selectedMessageId });
+    // }
+    //
+    // $scope.changeMessage = function (message) {
+    //     $scope.selectedMessageId = message.id;
+    //     console.log(MultipleViewsManager.isActive());
+    //     if (MultipleViewsManager.isActive()) {
+    //         MultipleViewsManager.updateView('view-message', { messageId: message.id });
+    //     } else {
+    //         $state.go('viewMessage', { messageId: message.id });
+    //     }
+    // };
+});
