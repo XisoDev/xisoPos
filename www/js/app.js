005019808c5328140a96071c822d5a3efefd9768
xisoDev <xiso@xiso.co.kr>
2016-10-13 19:06:16 +0900

    ionic init

---
+++ www/js/app.js
@@ -0,0 +1,40 @@
+angular.module('xisoPos', ['ionic', 'ionicMultipleViews'])
+
+	.run(function ($ionicPlatform) {
+		$ionicPlatform.ready(function () {
+			if (window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			}
+			
+			if (window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+		});
+	})
+
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('masterDetail', {
+				url: '/masterDetail',
+				templateUrl: 'templates/master-detail-layout.html',
+				abstract: true
+			})
+			
+			.state('masterDetail.currentCars', { 
+				url: '/currentCars',
+				views: {
+					'screen': {
+						templateUrl: 'templates/current_cars.html',
+						controller: 'CurrentCarsCtrl'
+					},
+					
+					'fixed-panel': {
+						templateUrl: 'templates/panel.html',
+						controller: 'PanelCtrl'
+					}
+				}
+			});
+
+		$urlRouterProvider.otherwise('/masterDetail/currentCars');
+	});
+
