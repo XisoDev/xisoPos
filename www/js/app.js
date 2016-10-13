04f08958c6d3b5bc3c49d6607e88c94ff5e167a4
xocode <overcode@xiso.co.kr>
2016-10-13 20:49:39 +0900

    중간점검

--- www/js/app.js
+++ www/js/app.js
@@ -19,7 +19,7 @@ angular.module('xisoPos', ['ionic', 'ionicMultipleViews'])
 				templateUrl: 'templates/master-detail-layout.html',
 				abstract: true
 			})
-			
+			//입출차 기록
 			.state('masterDetail.currentCars', { 
 				url: '/currentCars',
 				views: {
@@ -27,7 +27,67 @@ angular.module('xisoPos', ['ionic', 'ionicMultipleViews'])
 						templateUrl: 'templates/current_cars.html',
 						controller: 'CurrentCarsCtrl'
 					},
-					
+
+					'fixed-panel': {
+						templateUrl: 'templates/panel.html',
+						controller: 'PanelCtrl'
+					}
+				}
+			})
+			//월차
+			.state('masterDetail.monthCars', {
+				url: '/monthCars',
+				views: {
+					'screen': {
+						templateUrl: 'templates/month_cars.html',
+						controller: 'MonthCarsCtrl'
+					},
+
+					'fixed-panel': {
+						templateUrl: 'templates/panel.html',
+						controller: 'PanelCtrl'
+					}
+				}
+			})
+			//지정주차
+			.state('masterDetail.cooperCars', {
+				url: '/cooperCars',
+				views: {
+					'screen': {
+						templateUrl: 'templates/cooper_cars.html',
+						controller: 'CooperCarsCtrl'
+					},
+
+					'fixed-panel': {
+						templateUrl: 'templates/panel.html',
+						controller: 'PanelCtrl'
+					}
+				}
+			})
+			//정산
+			.state('masterDetail.calcu', {
+				url: '/calcu',
+				views: {
+					'screen': {
+						templateUrl: 'templates/calcu.html',
+						controller: 'CalcuCtrl'
+					},
+
+					'fixed-panel': {
+						templateUrl: 'templates/panel.html',
+						controller: 'PanelCtrl'
+					}
+				}
+			})
+			//설정
+			.state('masterDetail.setting', {
+				url: '/setting',
+				views: {
+					'screen': {
+						templateUrl: 'templates/setting.html',
+						controller: 'SettingCtrl'
+					},
+
 					'fixed-panel': {
 						templateUrl: 'templates/panel.html',
 						controller: 'PanelCtrl'
